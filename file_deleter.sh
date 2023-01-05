6fb62858fac167af5a81e458d191234b91678218
Njaso <jason749573@163.com>
2023-01-05 20:14:18 +0800

    second commit

---
+++ file_deleter.sh
@@ -0,0 +1,4 @@
+for ((i = 1; i <= 40; i++))
+do
+    rm file$i && echo file $i is deleted
+done
---
+++ file_maker.sh
@@ -0,0 +1,4 @@
+for ((i = 1; i <= 40; i++))
+    do
+        touch file$i && echo file $i is ok
+done
